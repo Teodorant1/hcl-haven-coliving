0204ff6c9350759e9bd92a0a148123b82cc249fa
Dusan <dusanbojanic1@gmail.com>
2024-03-27 14:13:12 +0100

    dashboard component installation

--- src/app/_components/user-auth-form.tsx
+++ src/app/_components/user-auth-form.tsx
@@ -22,15 +22,6 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
 
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
 
-  async function onSubmit(event: React.SyntheticEvent) {
-    event.preventDefault();
-    setIsLoading(true);
-
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 3000);
-  }
-
   const router = useRouter();
   const makeAccount = api.auth.Addaccount.useMutation({
     onSuccess: () => {
