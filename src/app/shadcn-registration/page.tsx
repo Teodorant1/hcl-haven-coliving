c38f547f205d487aef5fa2c8cee8781aa9d25178
Dusan <dusanbojanic1@gmail.com>
2024-03-27 15:18:39 +0100

    dashboard component

--- src/app/shadcn-registration/page.tsx
+++ src/app/shadcn-registration/page.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 export default function AuthenticationPage() {
   return (
     <div>
-      <div className="md:hidden">
+      <div className="hidden">
         <Image
           src="/examples/authentication-light.png"
           width={1280}
@@ -29,18 +29,18 @@ export default function AuthenticationPage() {
           className="hidden dark:block"
         />
       </div>
-      <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="container relative grid  h-[800px] flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0">
         <Link
           href="/examples/authentication"
           className={cn(
             buttonVariants({ variant: "ghost" }),
-            "absolute right-4 top-4 md:right-8 md:top-8",
+            "absolute right-4 right-8 top-4 top-8",
           )}
         >
           Login
         </Link>
-        <div className=" relative hidden h-screen flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-          <div className="absolute inset-0 bg-zinc-900" />
+        <div className="relative hidden h-screen  flex-col bg-muted p-10 text-white dark:border-r md:block lg:flex">
+          <div className="absolute inset-0  bg-zinc-900 " />
           <div className="relative z-20 flex items-center text-lg font-medium">
             <svg
               xmlns="http://www.w3.org/2000/svg"
