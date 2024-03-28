ac27d8fd10d818f997c576e40361f29e32bb47d5
Dusan <dusanbojanic1@gmail.com>
2024-03-28 21:11:53 +0100

    fixed supabase client

--- supabaseclient.ts
+++ supabaseclient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Initialize Supabase client
-const supabaseUrl = process.env.NEXT_PRIVATE_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PRIVATE_SUPABASE_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY!;
 export const supabase = createClient(supabaseUrl, supabaseKey);
