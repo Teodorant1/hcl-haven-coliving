1d3477e3831fcfa749ff3df8b1980beb925d80d7
Dusan <dusanbojanic1@gmail.com>
2024-08-07 21:16:06 +0200

    removed default tables and example pages

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "@/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
 import { authRouter } from "./routers/auth";
 import { bookingRouter } from "./routers/booking";
@@ -9,7 +8,6 @@ import { bookingRouter } from "./routers/booking";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   auth: authRouter,
   booking: bookingRouter,
 });
