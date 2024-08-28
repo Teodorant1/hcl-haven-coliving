8b19f469c035792e74d07c015e0f5f6c9b0bf0d3
Dusan <dusanbojanic1@gmail.com>
2024-08-28 20:58:41 +0200

    confirmed that prisma adapter is weird

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -12,7 +12,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 // import { useRouter } from "next/navigation";
-import { FcGoogle } from "react-icons/fc";
+// import { FcGoogle } from "react-icons/fc";
 import { signIn, useSession } from "next-auth/react";
 // import { signIn, useSession } from "next-auth/react";
 
